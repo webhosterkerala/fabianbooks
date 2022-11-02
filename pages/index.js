2ead152694c151e4fc8cddc8a8a8deb4852c9952
webhosterkerala <117272367+webhosterkerala@users.noreply.github.com>
2022-11-02 23:00:22 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Fabian Book!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Mavelikkara <code> Kerala </code>
         </p>
       </main>
 
